ffc6c8fb1584e0a1cd5e2f13342d02925809c67e
iagochaves <iagovieirachaves@gmail.com>
2022-11-02 18:29:38 -0300

    fix: SSR translations

--- app/next-i18next.config.js
+++ app/next-i18next.config.js
@@ -1,6 +1,9 @@
+const path = require('path');
+
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'pt', 'es'],
+    localePath: path.resolve('./public/locales'),
   },
 };
