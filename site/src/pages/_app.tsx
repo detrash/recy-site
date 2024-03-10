c63ad44c6a07fd82ac48d81e1796538af0e34a80
Yuri Mutti <muttiyuri@gmail.com>
2024-03-10 07:51:08 +0100

    feat: add ga

--- site/src/pages/_app.tsx
+++ site/src/pages/_app.tsx
@@ -1,26 +1,39 @@
-import { ApolloProvider } from '@apollo/client';
-import AOS from 'aos';
-import { NextIntlProvider } from 'next-intl';
-import type { AppProps } from 'next/app';
-import { useEffect } from 'react';
-import AppLayout from 'src/layout';
-import { apolloClient } from 'src/lib/apollo';
+import { ApolloProvider } from "@apollo/client";
+import AOS from "aos";
+import { NextIntlProvider } from "next-intl";
+import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import AppLayout from "src/layout";
+import { apolloClient } from "src/lib/apollo";
 
-import '../styles/globals.scss';
-import 'yet-another-react-lightbox/styles.css';
+import "../styles/globals.scss";
+import "yet-another-react-lightbox/styles.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     AOS.init({
       once: true,
-      disable: 'phone',
+      disable: "phone",
       duration: 700,
-      easing: 'ease-out-cubic',
+      easing: "ease-out-cubic",
     });
   }, []);
 
   return (
     <ApolloProvider client={apolloClient}>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script id="google-analytics">
+        {`
+         window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+         
+           gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+      </Script>
       <NextIntlProvider messages={pageProps?.messages || {}}>
         <AppLayout>
           <Component {...pageProps} />
