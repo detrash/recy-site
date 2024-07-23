f237dcd508c391b347a2e0a0ecac7780786f1c9a
edwardoranj <175904393+edwardoranj@users.noreply.github.com>
2024-07-23 11:51:29 +0900

    remove app

--- app/next-i18next.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'pt', 'es'],
-    localePath: path.resolve('./public/locales'),
-  },
-};
--- app/src/utils/formatNumber.ts
+++
@@ -1,5 +0,0 @@
-export function formatNumber(number: number, locale: string | undefined) {
-  const currentFormat = locale === 'en' ? 'en-US' : 'pt-BR';
-
-  return new Intl.NumberFormat(currentFormat).format(number);
-}
